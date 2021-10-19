eb82c2f32441de470c419bf4837803a7ef82b8f8
Edmi6163 <80919217+Edmi6163@users.noreply.github.com>
2021-10-19 11:25:24 +0200

    Update Script.bash

--- Script.bash
+++ Script.bash
@@ -6,8 +6,8 @@ echo  faccio l upgrade dei repo
 sudo apt upgrade
 echo lancio autoremove
 sudo apt autoremove -y
-echo installo micro e rimuvo nano
-sudo apt purge nano -y && sudo apt install micro -y
+echo installo micro e vim, rimuovo nano
+sudo apt purge nano -y && sudo apt install micro -y && sudo apt install vim
 echo installo java
 sudo apt install default-jre && sudo apt install default-jdk  -y
 echo installo R e cose varie
@@ -30,6 +30,9 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+echo installo chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
 echo installo vlc
 sudo apt install vlc -y
 echo installo discord
