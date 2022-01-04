14877cce63801d46759d4762cff8cb6a68038f21
Edmi6163 <francesco.mauro01@gmail.com>
2022-01-04 12:55:02 +0100

    changes

--- package-auto-installer-debian.bash
+++ package-auto-installer-debian.bash
@@ -24,6 +24,8 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+#echo install vscode
+sudo apt install visual-studio-code -y
 echo installing flatpak
 sudo apt install flatpak -y
 echo installing arduino ide
