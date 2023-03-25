25913361468b8f21dd7b635a5dd37881c20dd117
Francesco Mauro <mauro.francesco@educ.di.unito.it>
2023-03-25 13:15:35 +0100

    general clean up

--- installerDots/package-auto-installer-debian.bash
+++ installerDots/package-auto-installer-debian.bash
@@ -8,22 +8,13 @@ echo autoremoving
 sudo apt autoremove -y
 echo installing git
 sudo apt install git
-echo installing tmux
-sudo apt install tmux -y
-echo removing nano, installing micro and installing vim
+echo removing nano and installing vim
 sudo apt purge nano -y && sudo apt install vim
 echo installing java
 sudo apt install default-jre -y && sudo apt install default-jdk  -y
-echo installing okular
-echo installing gnome-boxes
-sudo apt install gnome-boxes -y
 echo installing vlc
 sudo apt install vlc -y
-echo installing sublime-text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text
 #echo install vscode
 sudo apt install visual-studio-code -y
 echo installing flatpak
